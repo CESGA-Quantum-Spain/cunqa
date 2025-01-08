0efc492ecf8ba23a2eeb06160e8c08e4c4822126
acarballido <varocarballido1996@gmail.com>
2025-01-08 13:34:33 +0100

    example_bin.cpp

--- src/utils/test.cpp
+++ src/utils/test.cpp
@@ -1,20 +1,47 @@
 #include <iostream>
 #include <string>
 
-void Log (std::string *message){
-    std::cout << message << "\n";
+void arr_fun(int* x []){
+
+    *x[0] = 3;
+    std::cout << "Dentro de la funcion: " << x[0] << "\n";
+
+     
 }
 
-int Multiply(int a, int b){
-    std::string m = "Multiply";
-    Log(&m);
-    return a*b;
+void t(char* args[]){
+
+    std::cout << "Hola \n";
+
 }
+
+
 
 int main(){
 
-    char* x = "Hola";
-    std::cout << Multiply(2,3) << "\n";
+    int w1 [3] = {1,2,3};
+    int w2 [3] = {4,5,6};
+    
+    //int* x1 = w1;
+    //int* x2 = w2;
+     
+
+    int* y []  = {w1,w2};
+    int v [] = {1,2};
+
+    arr_fun(y);
+
+    std::cout << y[0] << "\n";
+
+
+    std::cout << y << " " << &w1 << "\n";
+
+    char a = 'Hola';
+    char b = 'b';
+
+    char* ab[] = {&a,&b};
+
+    t(ab);
 
     return 0;
 }
