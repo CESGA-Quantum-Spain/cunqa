616b8493e9ea3a422f47ae5c689d76eca8b81080
L0AA6K6vJcXTl6saLDqmOC0L+rCz3TfxpXg2HECQ+pQ= <dexposito@login02.lan.cesga.es>
2025-06-26 10:24:15 +0200

    changed wake_up_c7_18 to a quick qraise

--- code_tests/wake_up_c7_18.sh
+++ code_tests/wake_up_c7_18.sh
@@ -5,6 +5,6 @@
 #SBATCH -c 1                # Cores per task requested
 #SBATCH -t 00:10:00         # Run time (hh:mm:ss) 
 #SBATCH --mem-per-cpu=6G    # Memory per core demanded
-#SBATCH --nodelist=c7-18    # Specific node (also -w ) 
+#SBATCH -w c7-18    # Specific node (also -w ) 
 
 echo "I'm awake! :)"
