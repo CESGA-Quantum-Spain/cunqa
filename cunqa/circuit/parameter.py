b2e67a35bf48f76487cb046e3da4a95e208bcd7a
D-Exposito <dexposito@cesga.es>
2025-09-12 11:29:22 +0200

    added file for constructing the parameter class

---
+++ cunqa/circuit/parameter.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import numpy as np
+import sympy
+from sympy import Symbol, Number
+import symengine.lib.symengine_wrapper as SymEng
+
+# SymPy can use SymEngine as a backend
+sympy.use_symengine = True
+
+
+class ParameterError(Exception):
+    """ Class for signaling errors with the Parameter class."""
+    pass
+
+class Parameter(Symbol):
+    """ """
+    def __init__(name, **assumptions):
+        super().__init__(name, **assumptions)
+
+        
