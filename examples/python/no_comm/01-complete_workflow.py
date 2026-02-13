983dfccdfa835f0be9e3eb13f3c2cbd01298b810
acarballido <varocarballido1996@gmail.com>
2026-02-13 13:17:02 +0100

    No-comm examples improvement and beautiful CUNQA docs main page

--- examples/python/no_comm/01-complete_workflow.py
+++ examples/python/no_comm/01-complete_workflow.py
@@ -8,28 +8,40 @@ from cunqa.qpu import qraise, get_QPUs, run, qdrop
 from cunqa.qjob import gather
 from cunqa.circuit import CunqaCircuit
 
-# Raise QPUs (allocates classical resources for the simulation job) and retrieve them using get_QPUs
-# If GPU execution is desired, just add "gpu = True" as another qraise argument
-family = qraise(2, "00:10:00", simulator = "Aer", co_located = True)
+try:
+    # Raise QPUs (allocates classical resources for the simulation job) and retrieve them using get_QPUs
+    # If GPU execution is desired, just add "gpu = True" as another qraise argument
+    family = qraise(2, "00:10:00", simulator = "Aer", co_located = True)
 
-qpus  = get_QPUs(co_located = True, family = family)
+    qpus  = get_QPUs(co_located = True, family = family)
 
-qc = CunqaCircuit(5)
-qc.h(0)
-qc.cx(0, 1)
-qc.measure_all()
+    # ---------------------------
+    # Circuit:
+    #  qc.q0   ─[H]───●────[M]─
+    #                 |      
+    #  qc.q1   ──────[X]───[M]─
+    # ---------------------------
+    qc = CunqaCircuit(5)
+    qc.h(0)
+    qc.cx(0, 1)
+    qc.measure_all()
 
-qjobs = run([qc, qc], qpus, shots = 10) # non-blocking call
+    qjobs = run([qc, qc], qpus, shots = 10) # non-blocking call
 
 
-results = gather(qjobs)
+    results = gather(qjobs)
 
-# Getting the counts
-counts_list = [result.counts for result in results]
+    # Getting the counts
+    counts_list = [result.counts for result in results]
 
-# Printing the counts
-for counts in counts_list:
-    print(f"Counts: {counts}")
+    # Printing the counts
+    for counts in counts_list:
+        print(f"Counts: {counts}")
 
-########## Drop the deployed QPUs ###########
-qdrop(family)
+    ########## Drop the deployed QPUs ###########
+    qdrop(family)
+    
+except Exception as error:
+    qdrop(family)
+    raise error
+
