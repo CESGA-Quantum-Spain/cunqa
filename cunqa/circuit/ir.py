b8bbae11992a4e33af84a615b203453c18cf29ca
acarballido <varocarballido1996@gmail.com>
2026-04-28 13:07:24 +0200

    Managed qiskit dependency in ir.py and qpu.py. Now set at convenience at build time. Enable by default

--- cunqa/circuit/ir.py
+++ cunqa/circuit/ir.py
@@ -2,10 +2,7 @@ from __future__ import annotations
 from functools import singledispatch
 import copy
 
-from qiskit import QuantumCircuit
-from qiskit.circuit import Parameter, ParameterExpression
-
-
+from cunqa.constants import CUNQA_USE_QISKIT_PY
 from cunqa.circuit import CunqaCircuit
 from cunqa.utils import generate_id
 from cunqa.logger import logger
@@ -36,147 +33,153 @@ def _(c: dict) -> dict:
     logger.warning("Circuit is already in IR format, returning it as is.")
     return c
 
-@to_ir.register
-def _(c: QuantumCircuit) -> dict:
-    """
-    Transforms a `qiskit.QuantumCircuit` to json `dict`.
 
-    Args:
-        c (qiskit.QuantumCircuit): circuit to transform to json.
+if CUNQA_USE_QISKIT_PY:
 
-    Return:
-        Json dict with the circuit information.
-    """
-    quantum_registers = {}
-    qinit = 0
-    for qr in c.qregs:
-        quantum_registers[qr.name] = list(range(qinit, qinit + qr.size))
-        qinit += qr.size
+    from qiskit import QuantumCircuit
+    from qiskit.circuit import Parameter
 
-    classical_registers = {}
-    cinit = 0
-    for cr in c.cregs:
-        classical_registers[cr.name] = list(range(cinit, cinit + cr.size))
-        cinit += cr.size
-    
-    json_data = {
-        "id": "QuantumCircuit_" + generate_id(),
-        "is_dynamic": False,
-        "instructions":[],
-        "sending_to":[],
-        "num_qubits":sum([q.size for q in c.qregs]),
-        "num_clbits": sum([c.size for c in c.cregs]),
-        "quantum_registers": quantum_registers,
-        "classical_registers": classical_registers, 
-        "params":[],
-        "component_comms": {}
-    }
+    @to_ir.register
+    def _(c: QuantumCircuit) -> dict:
+        """
+        Transforms a `qiskit.QuantumCircuit` to json `dict`.
 
-    for instruction in c.data:
-        if instruction.operation.name not in SUPPORTED_QISKIT_OPERATIONS:
-            raise ValueError(f"Instruction {instruction.operation.name} not supported for conversion.")
+        Args:
+            c (qiskit.QuantumCircuit): circuit to transform to json.
 
-        qreg = [r._register.name for r in instruction.qubits]
-        qubit = [q._index for q in instruction.qubits]
+        Return:
+            Json dict with the circuit information.
+        """
+        quantum_registers = {}
+        qinit = 0
+        for qr in c.qregs:
+            quantum_registers[qr.name] = list(range(qinit, qinit + qr.size))
+            qinit += qr.size
+
+        classical_registers = {}
+        cinit = 0
+        for cr in c.cregs:
+            classical_registers[cr.name] = list(range(cinit, cinit + cr.size))
+            cinit += cr.size
         
-        clreg = [r._register.name for r in instruction.clbits]
-        bit = [b._index for b in instruction.clbits]
+        json_data = {
+            "id": "QuantumCircuit_" + generate_id(),
+            "is_dynamic": False,
+            "instructions":[],
+            "sending_to":[],
+            "num_qubits":sum([q.size for q in c.qregs]),
+            "num_clbits": sum([c.size for c in c.cregs]),
+            "quantum_registers": quantum_registers,
+            "classical_registers": classical_registers, 
+            "params":[],
+            "component_comms": {}
+        }
 
-        if instruction.operation.name == "barrier":
-            pass
+        for instruction in c.data:
+            if instruction.operation.name not in SUPPORTED_QISKIT_OPERATIONS:
+                raise ValueError(f"Instruction {instruction.operation.name} not supported for conversion.")
 
-        elif instruction.operation.name == "measure":
-            json_data["instructions"].append({
+            qreg = [r._register.name for r in instruction.qubits]
+            qubit = [q._index for q in instruction.qubits]
+            
+            clreg = [r._register.name for r in instruction.clbits]
+            bit = [b._index for b in instruction.clbits]
+
+            if instruction.operation.name == "barrier":
+                pass
+
+            elif instruction.operation.name == "measure":
+                json_data["instructions"].append({
+                    "name":instruction.operation.name,
+                    "qubits":[quantum_registers[k][q] for k,q in zip(qreg, qubit)],
+                    "clbits":[classical_registers[k][b] for k,b in zip(clreg, bit)]
+                })
+
+            elif instruction.operation.name == "unitary":
+                json_data["instructions"].append({
+                    "name":instruction.operation.name, 
+                    "qubits":[quantum_registers[k][q] for k,q in zip(qreg, qubit)],
+                    "params":[[list(map(lambda z: [z.real, z.imag], row)) 
+                            for row in instruction.operation.params[0].tolist()]]
+                })
+
+            elif instruction.operation.name == "save_state":
+                json_data["instructions"].append({
+                    "name":instruction.operation.name, 
+                    "qubits":[quantum_registers[k][q] for k,q in zip(qreg, qubit)],
+                    "snapshot_type": instruction.operation._subtype,
+                    "label": instruction.operation.label
+                })
+            elif instruction.operation.name == "set_statevector":
+                json_data["instructions"].append({
                 "name":instruction.operation.name,
-                "qubits":[quantum_registers[k][q] for k,q in zip(qreg, qubit)],
-                "clbits":[classical_registers[k][b] for k,b in zip(clreg, bit)]
-            })
+                "qubits":list(range(sum([q.size for q in c.qregs]))),
+                "params": [
+                    list(map(lambda z: [z.real, z.imag],
+                    instruction.operation.params[0].tolist()))
+                    ]
+                })
 
-        elif instruction.operation.name == "unitary":
-            json_data["instructions"].append({
-                "name":instruction.operation.name, 
-                "qubits":[quantum_registers[k][q] for k,q in zip(qreg, qubit)],
-                "params":[[list(map(lambda z: [z.real, z.imag], row)) 
-                           for row in instruction.operation.params[0].tolist()]]
-            })
-
-        elif instruction.operation.name == "save_state":
-            json_data["instructions"].append({
-                "name":instruction.operation.name, 
-                "qubits":[quantum_registers[k][q] for k,q in zip(qreg, qubit)],
-                "snapshot_type": instruction.operation._subtype,
-                "label": instruction.operation.label
-            })
-        elif instruction.operation.name == "set_statevector":
-            json_data["instructions"].append({
-            "name":instruction.operation.name,
-            "qubits":list(range(sum([q.size for q in c.qregs]))),
-            "params": [
-                list(map(lambda z: [z.real, z.imag],
-                instruction.operation.params[0].tolist()))
-                ]
-            })
-
-        elif instruction.operation.name == "if_else":
-            json_data["is_dynamic"] = True
-
-            if not any([sub_circuit is None for sub_circuit in instruction.operation.params]):
-                raise ValueError("if_else instruction with \'else\' case is not supported for the "
-                                 "current version.")
-            else:
-                sub_circuit = [
-                    sub_circuit for sub_circuit in instruction.operation.params 
-                    if sub_circuit is not None
-                ][0]
-
-
-            if (condition := instruction.condition[1]) not in [0, 1]:
-                raise ValueError("Only 0 or 1 are accepted as conditions for classically controlled "
-                                "operations for the current version.")
-            
-            for re in c.qregs:
-                sub_circuit.add_register(re)
-
-            sub_instructions = to_ir(sub_circuit)["instructions"]
-
-            cc_instruction = {
-                "name": "cif",
-                "clbits": [classical_registers[k][b] for k,b in zip(clreg, bit)],
-                "instructions": sub_instructions,
-                "condition": condition
-                }
-            
-            json_data["instructions"].append(cc_instruction)
-
-        else:
-
-            instruction_params = [
-                str(param) if (isinstance(param, Parameter) or isinstance(param, Parameter)) else param 
-                for param in instruction.operation.params
-            ]
-        
-            instr = {"name":instruction.operation.name, 
-                     "qubits":[quantum_registers[k][q] for k,q in zip(qreg, qubit)],
-                     "params":instruction_params
-                    }
-            
-            if instruction.operation.condition != None:
-
-                if instruction.operation._condition[1] not in [1]:
-                    raise ValueError("Only 1 is accepted as condition for classicaly controlled "
-                                     "operations for the current version.")
-                    
-                name = instruction.operation.condition[0]._register.name
-                index = instruction.operation.condition[0]._index
-                cc_clbit = classical_registers[name][index]
-
+            elif instruction.operation.name == "if_else":
                 json_data["is_dynamic"] = True
-                json_data["instructions"].append({"name":"cif",
-                                            "clbits":[cc_clbit],
-                                            "instructions":[instr]
-                                            })
-            
-            else:
-                json_data["instructions"].append(instr)
 
-    return json_data
+                if not any([sub_circuit is None for sub_circuit in instruction.operation.params]):
+                    raise ValueError("if_else instruction with \'else\' case is not supported for the "
+                                    "current version.")
+                else:
+                    sub_circuit = [
+                        sub_circuit for sub_circuit in instruction.operation.params 
+                        if sub_circuit is not None
+                    ][0]
+
+
+                if (condition := instruction.condition[1]) not in [0, 1]:
+                    raise ValueError("Only 0 or 1 are accepted as conditions for classically controlled "
+                                    "operations for the current version.")
+                
+                for re in c.qregs:
+                    sub_circuit.add_register(re)
+
+                sub_instructions = to_ir(sub_circuit)["instructions"]
+
+                cc_instruction = {
+                    "name": "cif",
+                    "clbits": [classical_registers[k][b] for k,b in zip(clreg, bit)],
+                    "instructions": sub_instructions,
+                    "condition": condition
+                    }
+                
+                json_data["instructions"].append(cc_instruction)
+
+            else:
+
+                instruction_params = [
+                    str(param) if (isinstance(param, Parameter) or isinstance(param, Parameter)) else param 
+                    for param in instruction.operation.params
+                ]
+            
+                instr = {"name":instruction.operation.name, 
+                        "qubits":[quantum_registers[k][q] for k,q in zip(qreg, qubit)],
+                        "params":instruction_params
+                        }
+                
+                if instruction.operation.condition != None:
+
+                    if instruction.operation._condition[1] not in [1]:
+                        raise ValueError("Only 1 is accepted as condition for classicaly controlled "
+                                        "operations for the current version.")
+                        
+                    name = instruction.operation.condition[0]._register.name
+                    index = instruction.operation.condition[0]._index
+                    cc_clbit = classical_registers[name][index]
+
+                    json_data["is_dynamic"] = True
+                    json_data["instructions"].append({"name":"cif",
+                                                "clbits":[cc_clbit],
+                                                "instructions":[instr]
+                                                })
+                
+                else:
+                    json_data["instructions"].append(instr)
+
+        return json_data
